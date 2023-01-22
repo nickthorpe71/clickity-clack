efd8687ce1bffb203e733d6f2533e318e249bae3
Nick Thorpe <nickthorpe71@gmail.com>
2023-01-22 15:39:31 -0600

    change border to red

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -8,7 +8,7 @@ interface SignProps {
 const Sign: FC<SignProps> = ({ style = "", children }) => {
     return (
         <div
-            className={`bg-orange-300 border-8 border-t-orange-600 border-r-orange-800 border-b-orange-900 border-l-orange-700 shadow-lg shadow-stone-900 ${style}`}
+            className={`bg-orange-300 border-8 border-t-red-600 border-r-red-800 border-b-red-900 border-l-red-700 shadow-lg shadow-stone-900 ${style}`}
         >
             {children}
         </div>
